eecc7e8587f36766522e6751ade7ed95c36760b8
hdsjkdgh <310741749@qq.com>
2019-11-12 20:58:49 +0800

    添加user实体

---
+++ provider-user/src/main/java/com/zteict/SpringbootApplication.java
@@ -0,0 +1,10 @@
+package com.zteict;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+    public static void main(String[] args){
+        SpringBootApplication.run(SpringBootApplication.class,args);
+    }
+}
